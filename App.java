06c1a1817d548d557f7c139659f37b7beeeba425
patricksilva1 <patricksilva5160@gmail.com>
2022-12-01 15:50:01 -0300

    App with double

---
+++ App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println(SHA256Helper.hash("Hello world!"));
+        System.out.println(SHA256Helper.hash(SHA256Helper.hash("Hello world!")));
+    }
+}
